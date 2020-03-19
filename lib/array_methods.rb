3649ea3e63b48a4ca61ceb6ee926ba42b9116e16
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-19 16:09:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,6 +8,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  result = -999
+  array.length.times do |index|
+    if array[index] == value_to_find
+      return index
+    end
+  end
+  nil
  
 end
 
